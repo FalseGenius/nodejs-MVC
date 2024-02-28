8362ef3f9c92a4dbe7178d572bff53c93a0d9a87
FalseGenius <billionairejackal@gmail.com>
2024-02-28 17:28:04 +0500

    bug fix

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const {getContacts, getContact, createContact, updateContact, deleteContract} = require("../controllers/contactController");
 const router = express.Router();
 
-router.route('/').get(getContacts).get(getContact).post(createContact);
+router.route('/').get(getContacts).post(createContact);
 
-router.route('/:id').put(updateContact).delete(deleteContract);
+router.route('/:id').get(getContact).put(updateContact).delete(deleteContract);
 
 module.exports = router;
