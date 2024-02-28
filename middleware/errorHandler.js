6796bedf3cc93875a330484b6d17cc432042ee0d
FalseGenius <billionairejackal@gmail.com>
2024-02-28 12:56:32 +0500

    Error handler modified

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,25 @@
+const {constants} = require('../constants');
+
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
-    res.status(statusCode).send({message:err.message, stackTrace:err.stack})
-    // res.status(statusCode).send("Something broke");
+
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR:
+            const body = {title: "Validation failed", message:err.message, stackTrace:err.stack}
+            res.status(statusCode).send(body);
+        case constants.NOT_FOUND:
+            const notFound = {title: "Not found", message:err.message, stackTrace:err.stack}
+            res.status(statusCode).send(notFound);
+        case constants.UNAUTHORIZED:
+            const Unauthorized = {title: "Unauthorized", message:err.message, stackTrace:err.stack}
+            res.status(statusCode).send(Unauthorized);
+        case constants.FORBIDDEN:
+            const forbidden = {title: "Forbidden", message:err.message, stackTrace:err.stack}
+            res.status(statusCode).send(forbidden);
+        default:
+            break;
+    }   
+
 }
 
 module.exports =  errorHandler;
