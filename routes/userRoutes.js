1c6abec4dcda79881c7df50310cc465752f674a5
FalseGenius <billionairejackal@gmail.com>
2024-02-28 17:59:18 +0500

    User Routes/Controller/Model added

---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+// const {getContacts, getContact, createContact, updateContact, deleteContract} = require("../controllers/contactController");
+const router = express.Router();
+
+// router.route('/').get(getContacts).post(createContact);
+
+// router.route('/:id').get(getContact).put(updateContact).delete(deleteContract);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const router = require("./routes/contactRoutes");
+const contactRoutes = require("./routes/contactRoutes");
+const userRoutes = require("./routes/userRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const connectDB = require("./config/dbConnection");
 require("dotenv").config();
@@ -8,7 +9,8 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use('/api/contacts/', router);
+app.use('/api/contacts/', contactRoutes);
+app.use('/api/users/', userRoutes);
 app.use(errorHandler);
 connectDB();
 
